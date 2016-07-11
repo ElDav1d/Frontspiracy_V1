13e58a00f83b3daaea8168f755af0abc1c03fe2f
Mercedes Egido <meegido@gmail.com>
2016-07-11 14:03:23 +0200

    added gallery behavour to first portfolio item

--- js/custom.js
+++ js/custom.js
@@ -207,6 +207,16 @@ niceScrollInit();
 				}
 		
 		});
+		
+		$('.gallery').each(function() { // the containers for all your galleries
+		    $(this).magnificPopup({
+		        delegate: 'a', // the selector for gallery item
+		        type: 'image',
+		        gallery: {
+		          enabled:true
+		        }
+		    });
+		});
 	}
 
 	
